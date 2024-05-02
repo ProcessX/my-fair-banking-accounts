7bb1090729444da607b7291d8ba9813089db5b8a
ProcessX <luc2526@gmail.com>
2024-05-02 09:40:20 +0200

    first commit

---
+++ src/test/java/com/example/myfairbankingaccounts/MyFairBankingAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfairbankingaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFairBankingAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
